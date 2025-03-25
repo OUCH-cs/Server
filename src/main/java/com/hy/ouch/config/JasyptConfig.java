caf2aee1b22fcb9da1f7a2939c4d8f7eed6ea6ca
99hyuk <yh99cho1@gmail.com>
2025-03-26 00:02:41 +0900

    [CHORE] 설정파일 빈 등록

--- src/main/java/com/hy/ouch/config/JasyptConfig.java
+++ src/main/java/com/hy/ouch/config/JasyptConfig.java
@@ -5,7 +5,9 @@ import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class JasyptConfig {
 	@Value("${jasypt.encryptor.password")
 	private String encryptKey;
