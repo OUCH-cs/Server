da1e282e7e5518604579fa5d120042261ecc1cd1
kdy2224 <kdy2224@naver.com>
2025-04-08 02:41:38 +0900

    [CHORE] 사소한 코드 수정

--- src/main/java/com/onebridge/ouch/dto/user/request/UserUpdateRequest.java
+++ src/main/java/com/onebridge/ouch/dto/user/request/UserUpdateRequest.java
@@ -3,10 +3,8 @@ package com.onebridge.ouch.dto.user.request;
 import java.time.LocalDate;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class UserUpdateRequest {
 
 	private String loginId;
