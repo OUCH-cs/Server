1514b96d4bc48f2520d968dd46b2d83b1abe2e95
kdy2224 <kdy2224@naver.com>
2025-04-07 22:20:55 +0900

    [REFACT] user api 리팩토링

--- src/main/java/com/onebridge/ouch/dto/symptom/response/GetSymptomResponse.java
+++ src/main/java/com/onebridge/ouch/dto/symptom/response/GetSymptomResponse.java
@@ -2,10 +2,8 @@ package com.onebridge.ouch.dto.symptom.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class GetSymptomResponse {
 
--- src/main/java/com/onebridge/ouch/service/user/UserService.java
+++ src/main/java/com/onebridge/ouch/service/user/UserService.java
@@ -3,40 +3,38 @@ package com.onebridge.ouch.service.user;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.onebridge.ouch.apiPayload.code.error.UserErrorCode;
+import com.onebridge.ouch.apiPayload.exception.OuchException;
 import com.onebridge.ouch.converter.UserConverter;
-import com.onebridge.ouch.domain.Nation;
 import com.onebridge.ouch.domain.User;
 import com.onebridge.ouch.domain.enums.UserStatus;
-import com.onebridge.ouch.dto.user.request.MypageUserInfoUpdateRequest;
-import com.onebridge.ouch.dto.user.response.MypageUserInfoResponse;
 import com.onebridge.ouch.dto.user.response.UserInfoResponse;
 import com.onebridge.ouch.repository.nation.NationRepository;
 import com.onebridge.ouch.repository.user.UserRepository;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class UserService {
 
 	private final UserRepository userRepository;
 	private final NationRepository nationRepository;
 	private final UserConverter userConverter;
 
-	public UserService(UserRepository userRepository, NationRepository nationRepository, UserConverter userConverter) {
-		this.userRepository = userRepository;
-		this.nationRepository = nationRepository;
-		this.userConverter = userConverter;
-	}
-
+	//유저 조회(테스트용)
 	@Transactional(readOnly = true)
 	public UserInfoResponse getUserInfo(Long id) {
-		User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+		User user = userRepository.findById(id).orElseThrow(() -> new OuchException(UserErrorCode.USER_NOT_FOUND));
 
 		return userConverter.convertToUserInfoResponse(user);
 	}
 
+	//유저 탈퇴(비활성화)
 	@Transactional
 	public void deactivateUser(Long userId) {
 		User user = userRepository.findById(userId)
-			.orElseThrow(() -> new RuntimeException("User not found"));
+			.orElseThrow(() -> new OuchException(UserErrorCode.USER_NOT_FOUND));
 
 		User deactivatedUser = user.toBuilder()
 			.status(UserStatus.INACTIVE)
@@ -45,36 +43,11 @@ public class UserService {
 		userRepository.save(deactivatedUser);
 	}
 
+	//유저 삭제(테스트용)
 	@Transactional
 	public void deleteUser(Long id) {
-		User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+		User user = userRepository.findById(id).orElseThrow(() -> new OuchException(UserErrorCode.USER_NOT_FOUND));
 		userRepository.delete(user);
-	}
-
-	@Transactional(readOnly = true)
-	public MypageUserInfoResponse myPageGetUserInfo(Long userId) {
-		User user = userRepository.findById(userId)
-			.orElseThrow(() -> new RuntimeException("User not found"));
-		return userConverter.convertToMypageUserInfoResponse(user);
-	}
-
-	@Transactional
-	public void myPageUpdateUserInfo(Long userId, MypageUserInfoUpdateRequest request) {
-		User user = userRepository.findById(userId)
-			.orElseThrow(() -> new RuntimeException("User not found"));
-
-		Nation wantedNation = nationRepository.findById(request.getNationId())
-			.orElseThrow(() -> new RuntimeException("Nation not found"));
-
-		User updatedUser = user.toBuilder()
-			.nickname(request.getNickname())
-			.phoneNumber(request.getPhoneNumber())
-			.gender(request.getGender())
-			.email(request.getEmail())
-			.nation(wantedNation)
-			.build();
-
-		userRepository.save(updatedUser);
 	}
 }
 
