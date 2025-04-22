1e77e1a428b028b5b63d97d87152a8356337565d
kdy2224 <kdy2224@naver.com>
2025-04-23 00:58:44 +0900

    [CHORE] SelfDiagnosisController, SymptomController 에 스웨거 세부 설정 추가

--- src/main/java/com/onebridge/ouch/controller/selfDiagnosis/SelfDiagnosisController.java
+++ src/main/java/com/onebridge/ouch/controller/selfDiagnosis/SelfDiagnosisController.java
@@ -24,9 +24,12 @@ import com.onebridge.ouch.dto.selfDiagnosis.response.GetSymptomsOfDiagnosisRespo
 import com.onebridge.ouch.security.authorization.UserId;
 import com.onebridge.ouch.service.selfDiagnosis.SelfDiagnosisService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "자가진단표 API", description = "자가진단표 API 입니다.")
 @RestController
 @RequestMapping("/self-diagnosis")
 @RequiredArgsConstructor
@@ -35,6 +38,7 @@ public class SelfDiagnosisController {
 	private final SelfDiagnosisService selfDiagnosisService;
 
 	//자가진단표 생성
+	@Operation(summary = "자가진단표 생성 API", description = "자가진단표 생성 API 입니다.")
 	@PostMapping
 	public ResponseEntity<ApiResponse<DiagnosisCreateResponseDetailed>> createDiagnosis( //request dto 에 userid 지우기
 		@RequestBody @Valid DiagnosisCreateRequest request,
@@ -45,6 +49,7 @@ public class SelfDiagnosisController {
 	}
 
 	//(자가진단표)id로 자가진단표 조회
+	@Operation(summary = "자가진단표 조회 API", description = "자가진단표 조회 API 입니다.")
 	@GetMapping("/{diagnosisId}")
 	public ResponseEntity<ApiResponse<GetDiagnosisResponse>> getDiagnosisById(
 		@PathVariable Long diagnosisId,
@@ -55,6 +60,7 @@ public class SelfDiagnosisController {
 	}
 
 	//사용자 모든 자가진단표 조회
+	@Operation(summary = "특정 사용자의 모든 자가진단표 조회 API", description = "특정 사용자의 모든 자가진단표 조회 API 입니다.")
 	@GetMapping
 	public ResponseEntity<ApiResponse<List<GetDiagnosisByUserIdResponse>>> getAllDiagnosisByUserId(
 		@UserId Long userId
@@ -64,6 +70,7 @@ public class SelfDiagnosisController {
 	}
 
 	//자가진단표 삭제
+	@Operation(summary = "자가진단표 삭제 API", description = "자가진단표 삭제 API 입니다.")
 	@DeleteMapping("/{diagnosisId}")
 	public ResponseEntity<ApiResponse<Void>> deleteDiagnosis(@PathVariable Long diagnosisId,
 		@UserId Long userId
@@ -73,6 +80,7 @@ public class SelfDiagnosisController {
 	}
 
 	//특정 자가진단표의 증상 목록 조회
+	@Operation(summary = "특정 자가진단표의 증상 목록 조회 API", description = "특정 자가진단표의 증상 목록 조회 API 입니다.")
 	@GetMapping("/{diagnosisId}/symptoms")
 	public ResponseEntity<ApiResponse<GetSymptomsOfDiagnosisResponse>> getSymptomsOfDiagnosis(
 		@PathVariable Long diagnosisId,
@@ -83,6 +91,7 @@ public class SelfDiagnosisController {
 	}
 
 	//자가진단표 수정
+	@Operation(summary = "자가진단표 수정 API", description = "자가진단표 수정 API 입니다.")
 	@PutMapping("/{diagnosisId}")
 	public ResponseEntity<ApiResponse<DiagnosisUpdateResponse>> updateDiagnosis(@PathVariable Long diagnosisId,
 		@RequestBody @Valid DiagnosisUpdateRequest request,
@@ -93,6 +102,7 @@ public class SelfDiagnosisController {
 	}
 
 	//자가진단표에 증상 추가
+	@Operation(summary = "특정 자가진단표에 증상 추가 API", description = "특정 자가진단표에 증상 추가 API 입니다.")
 	@PostMapping("/{diagnosisId}/symptoms")
 	public ResponseEntity<ApiResponse<Void>> addSymptomsToSelfDiagnosis(@PathVariable Long diagnosisId,
 		@RequestBody @Valid AddSymptomsToDiagnosisRequest request,
--- src/main/java/com/onebridge/ouch/controller/symptom/SymptomController.java
+++ src/main/java/com/onebridge/ouch/controller/symptom/SymptomController.java
@@ -11,8 +11,11 @@ import com.onebridge.ouch.apiPayload.ApiResponse;
 import com.onebridge.ouch.dto.symptom.response.GetSymptomResponse;
 import com.onebridge.ouch.service.symptom.SymptomService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "증상 API", description = "증상 API 입니다.")
 @RestController
 @RequestMapping("/symptoms")
 @RequiredArgsConstructor
@@ -21,6 +24,7 @@ public class SymptomController {
 	private final SymptomService symptomService;
 
 	//증상 목록 조회
+	@Operation(summary = "증상 목록 조회 API", description = "증상 목록 조회 API 입니다.")
 	@GetMapping
 	public ResponseEntity<ApiResponse<List<GetSymptomResponse>>> getSymptomsList() {
 		List<GetSymptomResponse> list = symptomService.getSymptomsList();
