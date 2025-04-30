fd0b19de197ad1521a49bb22d06cbe56b0c6cbf0
kdy2224 <kdy2224@naver.com>
2025-04-30 23:28:18 +0900

    [REFACT] 건강상태 api 수정 -건강상태 조회, 수정 api 에서 healthStatusId 를 따로 받지 않도록 수정(HealthStatusController) -건강상태 생성(POST) api 삭제 및 SignUpService 에 건강상태 생성 로직 추가

--- src/main/java/com/onebridge/ouch/service/healthStatus/HealthStatusService.java
+++ src/main/java/com/onebridge/ouch/service/healthStatus/HealthStatusService.java
@@ -1,19 +1,13 @@
 package com.onebridge.ouch.service.healthStatus;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.onebridge.ouch.apiPayload.code.error.CommonErrorCode;
 import com.onebridge.ouch.apiPayload.code.error.HealthStatusErrorCode;
 import com.onebridge.ouch.apiPayload.exception.OuchException;
 import com.onebridge.ouch.converter.HealthStatusConverter;
 import com.onebridge.ouch.domain.HealthStatus;
-import com.onebridge.ouch.domain.User;
-import com.onebridge.ouch.dto.healthStatus.request.HealthStatusCreateRequest;
 import com.onebridge.ouch.dto.healthStatus.request.HealthStatusUpdateRequest;
-import com.onebridge.ouch.dto.healthStatus.response.DateAndDisease;
 import com.onebridge.ouch.dto.healthStatus.response.GetHealthStatusResponse;
 import com.onebridge.ouch.repository.healthStatus.HealthStatusRepository;
 import com.onebridge.ouch.repository.user.UserRepository;
@@ -28,22 +22,22 @@ public class HealthStatusService {
 	private final HealthStatusConverter healthStatusConverter;
 	private final HealthStatusRepository healthStatusRepository;
 
-	//건강상태 생성
-	@Transactional
-	public void createHealthStatus(HealthStatusCreateRequest request, Long userId) {
-		User user = userRepository.findById(userId)
-			.orElseThrow(() -> new OuchException(CommonErrorCode.MEMBER_NOT_FOUND));
-
-		HealthStatus healthStatus = healthStatusConverter.healthStatusCreateRequestToHealthStatus(request,
-			user);
-
-		healthStatusRepository.save(healthStatus);
-	}
+	// //건강상태 생성
+	// @Transactional
+	// public void createHealthStatus(HealthStatusCreateRequest request, Long userId) {
+	// 	User user = userRepository.findById(userId)
+	// 		.orElseThrow(() -> new OuchException(CommonErrorCode.MEMBER_NOT_FOUND));
+	//
+	// 	HealthStatus healthStatus = healthStatusConverter.healthStatusCreateRequestToHealthStatus(request,
+	// 		user);
+	//
+	// 	healthStatusRepository.save(healthStatus);
+	// }
 
 	//특정 건강상태 조회
 	@Transactional
-	public GetHealthStatusResponse getHealthStatus(Long healthStatusId, Long userId) {
-		HealthStatus healthStatus = healthStatusRepository.findByIdAndUserId(healthStatusId, userId)
+	public GetHealthStatusResponse getHealthStatus(Long userId) {
+		HealthStatus healthStatus = healthStatusRepository.findByUserId(userId)
 			.orElseThrow(() -> new OuchException(HealthStatusErrorCode.HEALTH_STATUS_NOT_FOUND));
 		return healthStatusConverter.healthStatusToGetHealthStatusResponse(healthStatus);
 	}
@@ -59,9 +53,8 @@ public class HealthStatusService {
 
 	//특정 건강상태 수정
 	@Transactional
-	public void updateHealthStatus(HealthStatusUpdateRequest request,
-		Long healthStatusId, Long userId) {
-		HealthStatus healthStatus = healthStatusRepository.findByIdAndUserId(healthStatusId, userId)
+	public void updateHealthStatus(HealthStatusUpdateRequest request, Long userId) {
+		HealthStatus healthStatus = healthStatusRepository.findByUserId(userId)
 			.orElseThrow(() -> new OuchException(HealthStatusErrorCode.HEALTH_STATUS_NOT_FOUND));
 
 		HealthStatus updatedHealthStatus = healthStatusConverter
@@ -70,12 +63,12 @@ public class HealthStatusService {
 		healthStatusRepository.save(updatedHealthStatus);
 	}
 
-	//특정 건강상태 삭제
-	@Transactional
-	public void deleteHealthStatus(Long healthStatusId, Long userId) {
-		HealthStatus healthStatus = healthStatusRepository.findByIdAndUserId(healthStatusId, userId)
-			.orElseThrow(() -> new OuchException(HealthStatusErrorCode.HEALTH_STATUS_NOT_FOUND));
-
-		healthStatusRepository.delete(healthStatus);
-	}
+	// //특정 건강상태 삭제
+	// @Transactional
+	// public void deleteHealthStatus(Long healthStatusId, Long userId) {
+	// 	HealthStatus healthStatus = healthStatusRepository.findByIdAndUserId(healthStatusId, userId)
+	// 		.orElseThrow(() -> new OuchException(HealthStatusErrorCode.HEALTH_STATUS_NOT_FOUND));
+	//
+	// 	healthStatusRepository.delete(healthStatus);
+	// }
 }
