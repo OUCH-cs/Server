65f687979d36a1096c1df550f7d87a5bd4d88197
99hyuk <yh99cho1@gmail.com>
2025-06-03 23:50:42 +0900

    [FIX] 의료기록과 건강상태 생성, 수정 시 필드에 null 허용하도록 수정

--- src/main/java/com/onebridge/ouch/dto/medicalRecord/request/MedicalRecordUpdateRequest.java
+++ src/main/java/com/onebridge/ouch/dto/medicalRecord/request/MedicalRecordUpdateRequest.java
@@ -9,18 +9,13 @@ import lombok.Getter;
 @Getter
 public class MedicalRecordUpdateRequest {
 
-	@NotNull(message = "Visit date is required.")
 	private LocalDate visitDate;
 
-	@NotBlank(message = "Visiting hospital is required.")
 	private String visitingHospital;
 
-	@NotBlank(message = "Medical subject is required.")
 	private String medicalSubject;
 
-	@NotBlank(message = "Symptoms are required.")
 	private String symptoms;
 
-	@NotBlank(message = "Treatment summary is required.")
 	private String treatmentSummary;
 }
